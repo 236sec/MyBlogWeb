d0db257158086cd0187f03ad5f4bce99c79880e2
236sec <popole159za@gmail.com>
2023-09-07 18:36:04 +0700

    Add github provider and set up callback function

--- app/(auth)/api/auth/[...nextauth]/route.ts
+++ app/(auth)/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@ import { connectMongoDB } from "@/lib/mongodb";
 import User from "@/models/user";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GitHubProvider from "next-auth/providers/github";
 
 interface UserCredentials { 
     username : string, 
@@ -10,6 +11,10 @@ interface UserCredentials {
 
 export const authOptions = {
     providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET
+          }),
         CredentialsProvider({
             name:"credentials",
             credentials: {
@@ -37,6 +42,22 @@ export const authOptions = {
         jwt: true,
     },
     secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+        async jwt({ token, account }) {
+            // Persist the OAuth access_token to the token right after signin
+            if (account) {
+              token.accessToken = account.access_token
+            }
+            console.log(token,account);
+            return token
+          },
+          async session({ session, token, user }) {
+            // Send properties to the client, like an access_token from a provider.
+            session.accessToken = token.accessToken
+            console.log(session,token,user);
+            return session
+          },
+      },
     //pages: {
     //    signIn: "/login",
     //},
--- app/components/Navbars.tsx
+++ app/components/Navbars.tsx
@@ -5,8 +5,9 @@ export default function NavBar(){
     return (
         <div className="m-0 mb-4 p-2 w-screen h-15 bg-black text-primary_light dark:text-white">
             <ol className="flex gap-5 text-2xl">
-                <li className=""><Link href={"/signin"}>Login</Link></li>
+                <li className=""><Link href={"/api/auth/signin"}>Login</Link></li>
                 <li className=""><Link href={"/signup"}>SignUp</Link></li>
+                <li className=""><Link href={"/dashboard"}>Dashboard</Link></li>
                 <li><ThemeSwitch /></li>
             </ol>
         </div>
