f63cde16a39a73c9995687cbb76dd36faff7eaaf
236sec <popole159za@gmail.com>
2023-10-01 22:21:42 +0700

    Fix import authOptions

--- app/api/restricted.ts
+++ app/api/restricted.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next"
-import { authOptions } from "./auth/[...nextauth]/route"
+import { authOptions } from "@/utils/authOptions"
 
 
 export default async function reti(req : any, res : any) {
